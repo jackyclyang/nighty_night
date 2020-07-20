312a2e54a0acb35e8f15cb32c1e7982d5d221a62
杨晨珑 <yangchenlong@yangchenlongdeMacBook-Pro.local>
2020-07-20 10:11:58 -0400

    2nd deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,6 @@
 
 User.destroy_all
 GreatThing.destroy_all
+ToDo.destroy_all
 
 @jacky = User.create!(name:'Jacky', email:'jacky@email.com', password:'123456')
-
-@jackyGreatThings = GreatThing.create!(content:'i saw a cute dog today', date:'July 10, 2020', user:@jacky)
-
-@jackyToDo = ToDo.create!(content:'finish part 2', status: 'incomplete', user:@jacky)
